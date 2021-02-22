1d3ab8cbcb2e056dbf50e0891b8f8b460ace278b
Romain Chahine <32666009+rChahine@users.noreply.github.com>
2021-02-22 17:07:04 +0100

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.authentication.routes import router as AuthRouter
 from api.upload.routes import router as UploadRouter
+from .settings import TESTING
 
 app = FastAPI(
     title="MSPR2"
@@ -8,3 +9,6 @@ app = FastAPI(
 
 app.include_router(AuthRouter)
 app.include_router(UploadRouter)
+
+if TESTING:
+    sys.exit(0)
