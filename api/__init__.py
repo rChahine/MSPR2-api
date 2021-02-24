18811bb3a123efc3bce5a796bef8ff57286b6bbe
Romain CHAHINE <romain.chahine@rwigo.com>
2021-02-24 15:23:32 +0100

    enable cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.authentication.routes import router as AuthRouter
 from api.upload.routes import router as UploadRouter
 
 app = FastAPI(
     title="MSPR2"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(AuthRouter)
