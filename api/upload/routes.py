4499a00d00b1e37b8f9d288f1cb72ec017730408
Romain CHAHINE <romain.chahine@rwigo.com>
2021-03-08 16:57:37 +0100

    🐛allow all MIME types

--- api/upload/routes.py
+++ api/upload/routes.py
@@ -21,53 +21,45 @@ async def upload_csv(
     user: User = Depends(get_user),
     session: Session = Depends(get_session)
 ):
-    print(file.content_type)
-    if file.content_type == "text/csv":
+    file_content = await file.read()
+    file_location = "tmp"
 
-        file_content = await file.read()
-        file_location = "tmp"
+    if not os.path.exists(file_location):
+        os.makedirs(file_location)
 
-        if not os.path.exists(file_location):
-            os.makedirs(file_location)
+    async with aiofiles.open(
+        f'{file_location}/{file.filename}',
+        'wb'
+    ) as out_file:
 
-        async with aiofiles.open(
-            f'{file_location}/{file.filename}',
-            'wb'
-        ) as out_file:
+        global file_id
 
-            global file_id
+        await out_file.write(file_content)  # async write
 
-            await out_file.write(file_content)  # async write
-
-            file_database = File_Upload(
-                id_user=user.id,
-                nom=file.filename
-            )
-
-            session.add(file_database)
-            session.commit()
-
-            file_id = file_database.id
-
-        nb_line = 0
-
-        with open(f'{file_location}/{file.filename}', newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',')
-            for row in spamreader:
-                if nb_line > 0:
-                    print(row)
-                    data = Data(
-                        id_file=file_id,
-                        information=row[1],
-                        valeur=row[2]
-                    )
-                    session.add(data)
-                    session.commit()
-
-                nb_line += 1
-
-    else:
-        raise HTTPException(
-            status_code=400,
-            detail="Invalid file MIME type"
+        file_database = File_Upload(
+            id_user=user.id,
+            nom=file.filename
         )
+
+        session.add(file_database)
+        session.commit()
+
+        file_id = file_database.id
+
+    nb_line = 0
+
+    with open(f'{file_location}/{file.filename}', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        for row in spamreader:
+            if nb_line > 0:
+                print(row)
+                data = Data(
+                    id_file=file_id,
+                    information=row[1],
+                    valeur=row[2]
+                )
+                session.add(data)
+                session.commit()
+
+            nb_line += 1
+
