7b265721f689fb098f1ce38fcf016d11fb23f2d8
Romain Chahine <32666009+rChahine@users.noreply.github.com>
2021-03-10 14:59:53 +0100

    Update tests.groovy

--- jenkins/tests.groovy
+++ jenkins/tests.groovy
@@ -4,28 +4,27 @@ pipeline {
     stages {
         stage('Setup config') {
             steps {
-                sh '''#!/bin/bash
-                    touch .env
-                    echo '
-                        DATABASE_URL=postgresql://uy5paos4qw9hdpg5mklq:1tjg9DNqUwyPzUQLMwvf@bk0gqvjcvowyhj2azddb-postgresql.services.clever-cloud.com:5432/bk0gqvjcvowyhj2azddb
-                        SECRET_KEY=zefuihzefizpaefhzoiefhzeiofhze2342ofhizefzoe
-                        TESTING=true
-                    ' > .env
+                bat '''
+
+                    type nul > .env
+                    
+                    echo DATABASE_URL=postgresql://uy5paos4qw9hdpg5mklq:1tjg9DNqUwyPzUQLMwvf@bk0gqvjcvowyhj2azddb-postgresql.services.clever-cloud.com:5432/bk0gqvjcvowyhj2azddb >> .env
+                    echo SECRET_KEY=zefuihzefizpaefhzoiefhzeiofhze2342ofhizefzoe >> .env
+                    echo TESTING=true >> .env
                 '''
             }
         }
         stage('Install latests dependencies') {
             steps {
-                sh '''#!/bin/bash
-                    sudo /etc/poetry/bin/poetry install
+                bat '''
+                    poetry install
                 '''
             }
         }
         stage('test') {
             steps {
                 sh '''#!/bin/bash
-                   echo "Running tests ..."
-                   sudo /etc/poetry/bin/poetry run pytest
+                   poetry run pytest
                 '''
             }
         }
